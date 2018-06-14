697a97074a963854ebc6d2339e4111985b38509e
Le Tuan Anh <lekoi9x@gmail.com>
2018-06-14 21:18:27 +0700

    optimize code

--- voz-1.py
+++ voz-1.py
@@ -1,8 +1,24 @@
+def isNumber(number):
+  return isinstance(number, (int, float))
+def getNumberFromInput(name):
+  title = "Please enter "+name+" number: "
+  while(True):
+    try:
+      data = float(raw_input(title))
+    except ValueError:
+      print "It's not number, Please type again"
+      continue
+    else:
+      break
+  return data
+def sum(number1, number2):
+  return number1 + number2
+
+
+  
 print "====== Hello World ======"
 print "Program: Sum Of Two Numbers"
-print "Please Enter First Number:"
-number1 = input()
-print "Please Enter Second Number:"
-number2 = input()
-result = number1 + number2
-print "Result %d + %d = %d" % (number1, number2, result)
+number1 = getNumberFromInput("first")
+number2 = getNumberFromInput("second")
+result = sum(number1, number2)
+print "Result %s + %s =  %s" % (number1, number2, result)
