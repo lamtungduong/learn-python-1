be7b0462962b79f8f5b42c2e6e82f97a6eae0e6f
Le Tuan Anh <lekoi9x@gmail.com>
2018-06-14 20:22:32 +0700

    ex1 voz1

---
+++ voz-1.py
@@ -0,0 +1,8 @@
+print "====== Hello World ======"
+print "Program: Sum Of Two Numbers"
+print "Please Enter First Number:"
+number1 = input()
+print "Please Enter Second Number:"
+number2 = input()
+result = number1 + number2
+print "Result %d + %d = %d" % (number1, number2, result)
